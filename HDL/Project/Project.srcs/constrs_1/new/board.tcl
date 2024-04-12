47ce3affd0c9f6b2304698781efed17531a8e538
CasperX <casperx102@gmail.com>
2024-04-13 00:32:29 +0700

    Update board.tcl

--- HDL/Project/Project.srcs/constrs_1/new/board.tcl
+++ HDL/Project/Project.srcs/constrs_1/new/board.tcl
@@ -1,39 +1,33 @@
 proc prop {obj pin iostandard} {
     # set package pin
     set_property PACKAGE_PIN $pin $obj
-    
+
     # set io standard
     set_property IOSTANDARD $iostandard $obj
 }
 
 proc setup {port pin iostandard} {
     set obj [get_ports $port]
-    
-    # set property 
+
+    # set property
     prop $obj $pin $iostandard
 }
 
-
-# -- clock
-
+# clock #
 set clk [get_ports sysclk]
 
 create_clock -add -name sys_clk_pin -period 10.0 -waveform {0 5} $clk
 
 prop $clk W5 LVCMOS33
 
-
-# -- input
-
+# inputs #
 setup reset T18 LVCMOS33
 
-
-# -- output
+# outputs #
 
 # 7 segments
-
 foreach {i pin} {7 V7 6 W7 5 W6 4 U8 3 V8 2 U5 1 V5 0 U7} {
-  setup seg\[$i\] $pin LVCMOS33
+    setup seg\[$i\] $pin LVCMOS33
 }
 
 foreach {i pin} {3 W4 2 V4 1 U4 0 U2} {
